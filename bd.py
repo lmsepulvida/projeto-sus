dd16cfceae22b9118da3f4fc4d432990f9679dc4
Guilherme Oliveira <guileoliveira@gmail.com>
2011-02-25 19:35:17 -0300

    Teste de adicao de paciente

---
+++ bd.py
@@ -0,0 +1,11 @@
+from datetime import date
+
+class Paciente():
+    def __init__(self, id, meu_nome, meu_seguro, minha_idade):
+	self.id = id        
+	self.nome = meu_nome
+        self.seguro = meu_seguro
+        self.idade = minha_idade
+        #self.StoragePacientes(self)
+
+	
---
+++ testes.py
@@ -0,0 +1,24 @@
+from should_dsl import should, should_not
+import unittest
+import paciente
+import specloud
+from datetime import date
+
+#INICIANDO TESTES USANDO SHOULD_DSL
+
+from bd import Paciente
+
+
+class Test_Paciente(unittest.TestCase):
+
+	def test_adicionar_novo_paciente(self):
+		self.paciente = Paciente(1,'Guilherme',2,3)
+		self.paciente.id |should| equal_to (1)
+		self.paciente.nome |should| equal_to("Guilherme")
+		self.paciente.seguro |should| equal_to(2)
+		self.paciente.idade |should| equal_to(3)	
+		#self.paciente.seguro |should| equal_to(1)
+	
+	
+if __name__ == "__main__":
+	unittest.main()
