53cfb395f17067e05d6c3fab284a44f2d270abb8
Guilherme Oliveira <guileoliveira@gmail.com>
2011-03-01 23:33:45 -0300

    quarto commit para adicao do cadastro de medicos

--- bd.py
+++ bd.py
@@ -1,11 +1,18 @@
 from datetime import date
 
 class Paciente():
-    def __init__(self, id, meu_nome, meu_seguro, minha_idade):
-	self.id = id        
-	self.nome = meu_nome
-        self.seguro = meu_seguro
-        self.idade = minha_idade
-        #self.StoragePacientes(self)
+	def __init__(self, id, meu_nome, meu_seguro, minha_idade):
+		self.id = id        
+		self.nome = meu_nome
+	        self.seguro = meu_seguro
+	        self.idade = minha_idade
+	        #self.StoragePacientes(self)
 
-	
+class Medico():
+	def __init__(self, id, meu_nome, minha_matricula, minha_especialidade):	
+		self.id = id
+		self.nome = meu_nome
+		self.matricula = minha_matricula
+		self.especialidade = minha_especialidade
+
+
--- testes.py
+++ testes.py
@@ -5,8 +5,7 @@ from datetime import date
 
 #INICIANDO TESTES USANDO SHOULD_DSL
 
-from bd import Paciente
-
+from bd import Paciente, Medico
 
 class Test_Paciente(unittest.TestCase):
 
@@ -17,7 +16,15 @@ class Test_Paciente(unittest.TestCase):
 		self.paciente.seguro |should| equal_to(2)
 		self.paciente.idade |should| equal_to(3)	
 		#self.paciente.seguro |should| equal_to(1)
-	
+
+class Test_Medico(unittest.TestCase):
+
+	def test_adicionar_novo_medico(self):
+		self.medico = Medico(1,'Roberto',2222,'clinico')
+		self.medico.id |should| equal_to (1)
+		self.medico.nome |should| equal_to("Roberto")
+		self.medico.matricula |should| equal_to(2222)
+		self.medico.especialidade |should| equal_to("clinico")	
 	
 if __name__ == "__main__":
 	unittest.main()
